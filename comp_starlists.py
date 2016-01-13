3d51359e886c3e7b82a3a503fc979b44bcbab104
Max Service <mws83@case.edu>
2016-01-13 09:47:20 -1000

    added function to compute yelda error statistics

--- comp_starlists.py
+++ comp_starlists.py
@@ -706,14 +706,14 @@ def test_yelda(pa1='mag07maylgs_kp_rms.lis', pa2='mag07maylgs_tran4_kp_rms_named
     N, x1m, y1m, m1m, x2m, y2m, m2m = jay.miracle_match_briteN(lis1['x'], lis1['y'], lis1['mag'], lis2['x'], lis2['y'], lis2['mag'], 50)
     t = high_order.four_paramNW(x1m, y1m, x2m, y2m)
     xn, yn = t.evaluate(lis1['x'], lis1['y'])
-    idx1, idx2 , dr, dm = align.match(xn, yn, lis1['mag'], lis2['x'], lis2['y'], lis2['mag'],dm_tol=1, .2)
+    idx1, idx2 , dr, dm = align.match(xn, yn, lis1['mag'], lis2['x'], lis2['y'], lis2['mag'], .2,dm_tol=1)
     #now recalculate 4 parameter tranformtion based onn more stars, but only bright ones (selected form first list, arbitrarily
     mc1 = lis1['mag'][idx1] < mag_cut
     #mc2 = lis2['mag'][idx2] < mag_cut
     t = high_order.four_paramNW(lis1['x'][idx1][mc1], lis1['y'][idx1][mc1], lis2['x'][idx2][mc1], lis2['y'][idx2][mc1])
 
     xn, yn = t.evaluate(lis1['x'], lis1['y'])
-    idx1, idx2 , dr, dm = align.match(xn, yn, lis1['mag'], lis2['x'], lis2['y'], lis2['mag'] , dm_tol=1, .2)
+    idx1, idx2 , dr, dm = align.match(xn, yn, lis1['mag'], lis2['x'], lis2['y'], lis2['mag'] ,  .2, dm_tol=1)
     mc1 = lis1['mag'][idx1] < mag_cut
     #mc2 = lis2['mag'][idx2] < mag_cut
 
